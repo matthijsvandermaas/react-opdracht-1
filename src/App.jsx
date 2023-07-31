8a44bb602ff92d26cf5e4ef5fa07942ba753a788
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-07-31 20:56:25 +0200

    bericht

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import './App.css';
 
 function App() {
     return (
         <main>
-            Begin hier met de tutorial!
+            <article>
+                <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" alt="a limon"/>
+
+                <h2>Citroen</h2>
+                <p>
+                    Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het
+                    vruchtvlees, het
+                    sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander
+                    voedsel.
+                </p>
+            </article>
         </main>
-    )
+    );
 }
 
-export default App
+export default App;
